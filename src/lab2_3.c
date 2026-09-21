66e16b1aad4e16a4aa164b723bc27094c340796a
nilsbriska-code <nils.briska@magebit.com>
2026-09-21 12:46:30 +0300

    labb2task3

--- src/lab2_3.c
+++ src/lab2_3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /*
     Task:
@@ -13,7 +14,15 @@
 
 int is_prime(int n) {
     // TODO: check if n is prime using loop up to sqrt(n)
-    return 0; // placeholder
+
+    int is_prime = 1;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            is_prime = 0;
+            break;
+        }
+    }
+    return is_prime;
 }
 
 int main(void) {
@@ -22,7 +31,20 @@ int main(void) {
     printf("Enter an integer n (>= 2): ");
     scanf("%d", &n);
 
-    // TODO: validate input and print all primes up to n
+    // TODO: validate input and print all primes up to 
+    
+    if (n < 2) {
+        printf("Invalid input! Input an integer that is larger than 2\n");
+    }
+    else {
+        for (int i = 2; i <= n; i++) 
+        {
+            if(is_prime(i)) 
+            {
+                printf("%d\n", i);
+            }
+        }
+    }
 
     return 0;
 }
